543d2a47412203b5d14e9ec272fa0da1907a60a7
msaeiid <mskarbaschian@gmail.com>
2024-03-20 14:52:06 +0330

    8- The Tweets Model

--- todo.md
+++ todo.md
@@ -3,7 +3,7 @@
 1. Tweets
     -> Creating
         -> Text
-        -> Image
+        -> Image -> Media Storage Server
     -> Delete
     -> Retweeting
 
--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Tweet(models.Model):
+    content = models.TextField(null=True, blank=True)
+    # in tutorial he used FileField
+    image = models.ImageField(upload_to='tweets/', blank=True, null=True)
