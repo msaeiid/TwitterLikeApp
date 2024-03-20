1b5499536409d6c33487008bffa71d64af120cd7
msaeiid <mskarbaschian@gmail.com>
2024-03-20 22:45:54 +0330

    6-Update VS Code Config working with templates....

--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render
 from .models import Tweet
 import json
 
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, template_name="pages/home.html", context={})
 
 
 def tweet_detail_view(request, pk, *args, **kwargs):
