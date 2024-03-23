16da9e8cba5528fe1768a7bd213e4b5eabbea89e
msaeiid <mskarbaschian@gmail.com>
2024-03-23 13:34:05 +0330

    2:57:09 34-Django Admin

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Tweet
 
-admin.site.register(Tweet)
+
+class CustomTweet(admin.ModelAdmin):
+    list_display = ['__str__', 'user']
+    search_fields = ['user__username', 'user__email']
+
+
+admin.site.register(Tweet, CustomTweet)
