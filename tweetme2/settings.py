a12bf3cbea3cf2e19a47206893a857dbf91c4840
msaeiid <mskarbaschian@gmail.com>
2024-03-23 15:04:03 +0330

    3:16:16 37-Install Django Rest Framework

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party
+    'rest_framework',
+    # Internal
     'tweets.apps.TweetsConfig'
 ]
 
