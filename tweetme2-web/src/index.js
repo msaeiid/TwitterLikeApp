791a652cf3cdcba5ae62f8558285d658bcd3b756
msaeiid <mskarbaschian@gmail.com>
2024-03-28 16:48:15 +0330

    6:19:11 63-Adjust the React Render Process

--- tweetme2-web/src/index.js
+++ tweetme2-web/src/index.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {TweetComponent}  from './tweets'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const appEl=document.getElementById('root')
+if (appEl) {
+  const root = ReactDOM.createRoot(appEl);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ); 
+}
+
+const tweetEl=document.getElementById('tweetme2')
+if (tweetEl) {
+  const root = ReactDOM.createRoot(tweetEl);
+  root.render(
+    <React.StrictMode>
+      <TweetComponent />
+    </React.StrictMode>
+  ); 
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
