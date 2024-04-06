61478c2e08451b4a7f4843ab284195fbf1f5e508
msaeiid <mskarbaschian@gmail.com>
2024-04-06 23:24:30 +0330

    9:15:32 87-Signals to Create Profiel Objects

--- profiles/models.py
+++ profiles/models.py
@@ -8,3 +8,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=220, null=True, blank=True)
     bio = models.TextField(blank=True,null=True)
+
+    def __str__(self) -> str:
+        return self.user.username
