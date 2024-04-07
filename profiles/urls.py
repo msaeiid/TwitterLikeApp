1c3cc2e0323f0767d3de5765761a50632cf494e4
msaeiid <mskarbaschian@gmail.com>
2024-04-07 11:36:09 +0330

    9:21:44 88-Save 2 Models in 1 Form and 1 View

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import profile_detail_view
+from .views import profile_detail_view, profile_update_view
 urlpatterns = [
     path('<str:username>/', profile_detail_view, name='profile'),
+    path('update/edit', profile_update_view, name='profile_update')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile
 from django.http import Http404
+from .forms import ProfileForm
+from django.urls import reverse
+
+
+def profile_update_view(request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        return redirect(reverse('login'), next=reverse('profile_update'))
+
+    user_ = request.user
+    profile_ = user_.profile
+    initial_ = {
+        'first_name': user_.first_name,
+        'last_name': user_.last_name,
+        'email': user_.email,
+        'bio': profile_.bio,
+        'location': profile_.location
+    }
+    form = ProfileForm(data=request.POST or None,
+                       instance=profile_, initial=initial_)
+    if form.is_valid():
+        form_obj = form.save(commit=False)
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        email = form.cleaned_data.get('email')
+        user_.first_name = first_name
+        user_.last_name = last_name
+        user_.email = email
+        user_.save()
+        form_obj.save()
+    context = {
+        'form': form,
+        'btn_label': 'Update Profile',
+        'title': 'Update Profile'
+    }
+    return render(request, 'profiles/update.html', context=context)
+
 
 def profile_detail_view(request, username, *args, **kwargs):
     qs = Profile.objects.filter(user__username=username)
