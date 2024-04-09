7f775881b862042ba76abbe05919e0a9f01bfff2
msaeiid <mskarbaschian@gmail.com>
2024-04-09 13:08:16 +0330

    9:46:47 90-Followers and Following

--- profiles/models.py
+++ profiles/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 from django.conf import settings
-
 User = settings.AUTH_USER_MODEL
+
+
+class FollowRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=220, null=True, blank=True)
     bio = models.TextField(blank=True,null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    followers = models.ManyToManyField(
+        User, through=FollowRelation, related_name='following', blank=True)
+    """project_obj = Profile.objects.first()
+       project_obj.followers.all() -> All users following this profile
+       user.following.all() -> All profiles I follow
+    """
+
+
 
     def __str__(self) -> str:
         return self.user.username
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+class ProfileTestCase(TestCase):
+    def setUp(self) -> None:
+        self.first_user = User.objects.create(
+            username='first_user', password='213')
+        self.second_user = User.objects.create(
+            username='second_user', password='123')
+
+    def test_profile_created_via_signal(self):
+        qs = User.objects.count()
+        self.assertEqual(qs, 2)
+
+    def test_following(self):
+
+        self.first_user.profile.followers.add(self.second_user)
+        # add a follower ...
+        qs = self.second_user.following.filter(user=self.first_user)
+        # from a user, checked user is being followed
+        who_first_user_followed = self.first_user.following
+        # check first user has no following
+        self.assertTrue(qs.exists())
+        self.assertFalse(who_first_user_followed.exists())
