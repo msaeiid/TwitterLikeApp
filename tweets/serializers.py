b6120a9c13041681718b446ce879f424d109fb16
msaeiid <mskarbaschian@gmail.com>
2024-04-10 19:07:04 +0330

    10:38:27 96-User Profile Serializer

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
 from .models import Tweet
 from django.conf import settings
-
+from profiles.serializers import ProfileSerializer
 MAX_TWEET_LENGTH = settings.MAX_TWEET_LENGTH
 
 TWEET_ACTION_OPTIONS = settings.TWEET_ACTION_OPTIONS
 
 
 class TweetCreateSerializer(serializers.ModelSerializer):
+    author = ProfileSerializer(source='user.profile', read_only=True)
     likes = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Tweet
-        fields = ['id', 'content', 'likes']
+        fields = ['id', 'content', 'likes', 'author']
 
     def validate_content(self, value):
         if len(value) > MAX_TWEET_LENGTH:
@@ -23,6 +24,7 @@ class TweetCreateSerializer(serializers.ModelSerializer):
 
 
 class TweetSerializer(serializers.ModelSerializer):
+    author = ProfileSerializer(source='user.profile', read_only=True)
     likes = serializers.SerializerMethodField(read_only=True)
     parent = TweetCreateSerializer(read_only=True)
     """if in some cases like below I want serializer field be different from model field -> og_tweet -> source='parent' is user like below
@@ -31,7 +33,7 @@ class TweetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tweet
-        fields = ['id', 'content', 'likes', 'is_retweet', 'parent']
+        fields = ['author', 'id', 'content', 'likes', 'is_retweet', 'parent']
 
     def get_likes(self, obj):
         return obj.likes.count()
