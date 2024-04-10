0e51f6e2dd944d8fbcbe28865861cb3b5bccdb62
msaeiid <mskarbaschian@gmail.com>
2024-04-10 15:10:49 +0330

    10:21:49 94-More Efficient Backend Lookups and Custom Model Managers

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import random
 from django.conf import settings
+from django.db.models import Q
 
 User = settings.AUTH_USER_MODEL
 
@@ -15,6 +16,30 @@ class TweetLike(models.Model):
 
     def __str__(self) -> str:
         return f'{self.pk}'
+
+
+class TweetQuerySet(models.QuerySet):
+    # it query comes after all()
+    def feed(self, user):
+        profiles_exit = user.following.exists()
+        if profiles_exit:
+            follower_users_id = user.following.values_list(
+                'user__id', flat=True)
+            return self.filter(
+                Q(user__id__in=follower_users_id) |
+                Q(user=user)
+            ).distinct().order_by('-timestamp')
+
+
+class TweetManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet:
+        return TweetQuerySet(self.model, using=self._db)
+
+    # it query comes after objects()
+    def feed(self, user):
+        return self.get_queryset().feed(user)
+
+
 class Tweet(models.Model):
     # it is used for retweeting
     parent = models.ForeignKey("self", on_delete=models.SET_NULL, null=True)
@@ -36,3 +61,5 @@ class Tweet(models.Model):
     @property
     def is_retweet(self):
         return self.parent is not None
+
+    objects = TweetManager()
