4866b995ef8ec6f4b0a71d2491761f5e78188e29
msaeiid <mskarbaschian@gmail.com>
2024-04-15 19:34:15 +0330

    12:27:53 109-Final Build

---
+++ tweetme2-web/build/precache-manifest.2c1f2df6cc89c636a5b37ac2b69722b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd40e57a155d25e98e2a7ca4c36934d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70d51bdb9a25dbef5ef1",
+    "url": "/static/css/main.680b16f9.chunk.css"
+  },
+  {
+    "revision": "65b77deb8c52aa9f68ee",
+    "url": "/static/js/2.11c1c52a.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "70d51bdb9a25dbef5ef1",
+    "url": "/static/js/main.28831945.chunk.js"
+  },
+  {
+    "revision": "e21c8a56d64ede50141d",
+    "url": "/static/js/runtime~main.891e0007.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
